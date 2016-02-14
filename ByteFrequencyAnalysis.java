424a53e1bd3b24d026d575910188a3e29e587f1d
prasadns14 <prasadns14@gmail.com>
2016-02-14 10:41:40 -0800

    Updated Readme

--- ByteFrequencyAnalysis.java
+++ ByteFrequencyAnalysis.java
@@ -4,30 +4,85 @@ import java.util.Scanner;
 public class ByteFrequencyAnalysis {
 
     public static void main(String[] args) throws Exception{
-        File file = new File("C:\\Users\\prasa\\Desktop\\ResumePrasadNS.pdf");
-        File fingerprint = new File("C:\\Users\\prasa\\Desktop\\TextPlain.txt");
-        FileInputStream fIS = new FileInputStream(file);
-        Scanner sc = new Scanner(new FileReader(fingerprint));
-        int numFiles = sc.nextInt();
+        
+		public static final String[] fileTypes = {"text\\plain", "text\\pdf", "text\\rdf+xml", "text\\rsf+xml", "text\\xhtml+xml", "text\\html", "image\\png", "image\\jpeg", "audio\\mpeg", "video\\mp4", "video\\quicktime", "applicaiton\\x-sh", "application\\gzip", "application\\msword", "application\\octet-stream"};
 
+    public static void main(String[] args) throws Exception{
 
-        byte[] byteFile = new byte[(int) file.length()];
-        fIS.read(byteFile);
-        fIS.close();
-        int[] byteCount = new int[256];
-        for(int i = 0; i < byteFile.length; i++){
-            byteCount[0xFF & byteFile[i]]++;
-        }
-        int max = 0;
-        for(int i = 0; i < byteCount.length; i++){
-            if(byteCount[i] > max){
-                max = byteCount[i];
+        for(int i = 0; i < fileTypes.length; i++) {
+
+            //Selecting the file type and setting the path to the folder which has the data and fingerprint file corresponding to that file type
+            File fileType = new File("D:\\PolarDump\\"+fileTypes[i]+"\\data");
+            String fingerprintFile = fileTypes[i].replace("\\","-");
+            File fingerprint = new File("D:\\PolarDump\\"+fileTypes[i]+"\\"+fingerprintFile+".txt");
+            File[] files = fileType.listFiles();
+
+            //Reading over each file of the selected type
+            for(File file : files) {
+                FileInputStream fIS = new FileInputStream(file);
+                Scanner sc = new Scanner(new FileReader(fingerprint));
+                int numFiles = sc.nextInt();
+                System.out.println(numFiles);
+                sc.nextLine();
+                String[] terms = sc.nextLine().split("\\t");
+                double[] byteFreq = new double[256];
+                double[] byteCorrelation = new double[256];
+                for (int j = 0; j < 256; j++) {
+                    String[] values = terms[j].split(",");
+                    byteFreq[j] = Double.parseDouble(values[0]);
+                    byteCorrelation[j] = Double.parseDouble(values[1]);
+                }
+                byte[] byteFile = new byte[(int) file.length()];
+                fIS.read(byteFile);
+                fIS.close();
+                double[] byteCount = new double[256];
+
+                //calculating its byte frequency distribution
+                for (int j = 0; j < byteFile.length; j++) {
+                    byteCount[0xFF & byteFile[j]]++;
+                }
+
+                //Finding the max occurring byte
+                double max = byteCount[0];
+                int count = 0;
+                for (int j = 1; j < byteCount.length; j++) {
+                    if (byteCount[j] > max) {
+                        max = byteCount[j];
+                    }
+                    count += byteCount[j];
+                }
+
+                //Normalizing the byte frequency
+                for (int j = 0; j < byteCount.length; j++) {
+                    byteCount[j] = byteCount[j] / max;
+                }
+
+                //Compounding the byte frequency if needed
+                if (max > (0.7 * count)) {
+                    for (int j = 0; j < 256; j++) {
+                        byteCount[j] = Math.pow(byteCount[j], 0.5);
+                    }
+                }
+
+                //Updating the fingerprint file
+                for (int j = 0; j < 256; j++) {
+                    byteFreq[j] = ((byteFreq[j] * numFiles) + byteCount[j]) / (numFiles + 1);
+                }
+
+                sc.close();
+                FileWriter fw = new FileWriter(fingerprint);
+                fw.write(Integer.toString(numFiles + 1));
+
+                fw.write("\n");
+                for (int j = 0; j < 256; j++) {
+                    String temp = byteFreq[j] + "," + byteCorrelation[j] + "\t";
+                    fw.write(temp);
+                }
+                fw.write("\n");
+                fw.close();
             }
         }
-        System.out.println(max);
-        for(int i = 0; i < byteCount.length; i++){
-            System.out.println(i+" " + (byteCount[i]*1.0/max));
-        }
+    }
 
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # team9
+
+Steps to be followed
+1) Create a folder structure for each selected file type as (<mime-type/sub-mime-type>)
+2) Copy the fingerprint file for that file type to the folder<(<mime-type-sub-mime-type>.txt)
+3) Copy all the files of that type under a folder data created inside the folder
+4) Now, run the ByteFrequencyAnalysis.java to run the step a of the assignment 
